959f3a0443937f4aaf17a6eb84519d7ac056315a
Jeff Horton <jeff@goblinoid.co.uk>
2015-09-09 18:45:49 +0100

    Tabs + invoke test

--- test/Text/ArrayAsAlignedStringTest.php
+++ test/Text/ArrayAsAlignedStringTest.php
@@ -1,45 +1,56 @@
 <?php
-	/**
-	 * Created by PhpStorm.
-	 * User: jeff
-	 * Date: 09/09/2015
-	 * Time: 12:44
-	 */
-	
-	namespace Goblinoid\Util\Text;
-	
-	
-	class ArrayAsAlignedStringTest extends \PHPUnit_Framework_TestCase
-	{
-		/**
-		 * @param $array
-		 * @param $expected
-		 * @dataProvider getTestCases
-		 */
-		public function testFiltersTestCases($array, $expected)
-		{
+    /**
+     * Created by PhpStorm.
+     * User: jeff
+     * Date: 09/09/2015
+     * Time: 12:44
+     */
+    
+    namespace Goblinoid\Util\Text;
+    
+    
+    class ArrayAsAlignedStringTest extends \PHPUnit_Framework_TestCase
+    {
+        /**
+         * @param $array
+         * @param $expected
+         * @dataProvider getTestCases
+         */
+        public function testFiltersTestCases($array, $expected)
+        {
             $filter = new ArrayAsAlignedString();
             $this->assertEquals($expected, $filter->apply($array));
-		}
+        }
+        
+        /**
+         * @param $array
+         * @param $expected
+         * @dataProvider getTestCases
+         */
+        public function testInvokeTestCases($array, $expected)
+        {
+            $filter = new ArrayAsAlignedString();
+            $this->assertEquals($expected, $filter($array));
+        }
 
-		public function getTestCases()
-		{
-			return [
-				'Basic Usage' => [
-					'array' => ['Hello' => 'World'],
-				    'expected' => 'Hello: World',
-				],
-				'Indents Correctly' => [
-					'array' => ['Hello' => 'World', 'Much Longer Key' => 'Has a Value'],
-				    'expected' => 'Hello:           World' . PHP_EOL .
+        public function getTestCases()
+        {
+            return [
+                'Basic Usage' => [
+                    'array' => ['Hello' => 'World'],
+                    'expected' => 'Hello: World',
+                ],
+                'Indents Correctly' => [
+                    'array' => ['Hello' => 'World', 'Much Longer Key' => 'Has a Value'],
+                    'expected' => 'Hello:           World' . PHP_EOL .
                                   'Much Longer Key: Has a Value',
-				],
-				'MB Safe' => [
-					'array' => ['Hello' => 'World', '€' => 'Euro', '€€€€€€€' => 'Euros'],
-				    'expected' => 'Hello:   World' . PHP_EOL .
-                                  '€:       Euro' . PHP_EOL .
-                                  '€€€€€€€: Euros',
-				],
+                ],
+                'MB Safe' => [
+                    'array' => ['Hello' => 'World', 'ï¿½' => 'Euro', 'ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½' => 'Euros'],
+                    'expected' => 'Hello:   World' . PHP_EOL .
+                                  'ï¿½:       Euro' . PHP_EOL .
+                                  'ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½: Euros',
+                ],
                 'Indents subsequent lines - basic' => [
                     'array' => ['Hello' => 'World' . PHP_EOL . 'Universe'],
                     'expected' => 'Hello: World' . PHP_EOL .
@@ -52,6 +63,6 @@
                                   '                 Universe' . PHP_EOL .
                                   'Much Longer Key: Has a Value',
                 ],
-			];
-		}
-	}
+            ];
+        }
+    }
